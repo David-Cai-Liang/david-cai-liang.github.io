c1bb105dc1b5dc8bb14d911cb77acba0907bbb9e
David Cai Liang <dac828@lehigh.edu>
2025-06-14 16:50:34 -0400

    stablize

--- contact.js
+++
@@ -1,6 +0,0 @@
-function openEmail() {
-    var email = 'davidcailiang9@gmail.com';
-    var subject = 'Question';
-    var stuff = 'Hello,\n\nI have a question:';
-    window.location.href = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(stuff);
-}
