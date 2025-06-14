8de90cc9760e3adc8ac8f3984783d0a4fbaecff3
David Cai Liang <dac828@lehigh.edu>
2025-06-14 16:34:31 -0400

    experiment 1

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const htmlElement = document.documentElement;
 function setTheme(theme) {
     htmlElement.setAttribute('data-theme', theme);
     localStorage.setItem('theme', theme);
-    themeToggle.innerHTML = theme === 'light' ? "Toggle Dark Mode" : "Toggle Light Mode";
+    themeToggle.innerHTML = theme === 'light' ? "🌙" : "☀️";
 }
 
 themeToggle.addEventListener('click', () => {
