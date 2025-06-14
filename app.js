4687d29b8fee6d92083ee89ff2b6c85a1416e48e
David Cai Liang <dac828@lehigh.edu>
2025-06-14 16:20:40 -0400

    add dark mode

---
+++ app.js
@@ -0,0 +1,28 @@
+function openEmail() {
+    var email = 'davidcailiang9@gmail.com';
+    var subject = 'Question';
+    var stuff = 'Hello,\n\nI have a question:';
+    window.location.href = 'mailto:' + email + '?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(stuff);
+}
+
+const themeToggle = document.getElementById('theme-toggle');
+const htmlElement = document.documentElement;
+
+function setTheme(theme) {
+    htmlElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+    themeToggle.innerHTML = theme === 'light' ? "Toggle Dark Mode" : "Toggle Light Mode";
+}
+
+themeToggle.addEventListener('click', () => {
+    const currentTheme = htmlElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    setTheme(newTheme);
+});
+
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+    setTheme(storedTheme);
+} else if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    setTheme('dark');
+}
