ab90caeba2ef0cbe44ac8176ef93130c01c64590
David Cai Liang <dac828@lehigh.edu>
2025-06-14 21:05:33 -0400

    close all

--- test.js
+++ test.js
@@ -11,4 +11,29 @@ for (i = 0; i < coll.length; i++) {
       content.style.display = "block";
     }
   });
-} 
+}
+
+var toggleAllBtn = document.getElementById("toggleAll");
+var isAllOpen = false; // State tracker
+
+// Toggle All button behavior
+toggleAllBtn.addEventListener("click", function () {
+  isAllOpen = !isAllOpen; // Flip state
+  if (isAllOpen) {
+      toggleAllBtn.textContent = "CLOSE ALL EXPERIENCES";
+  }
+  else {
+    toggleAllBtn.textContent = "OPEN ALL EXPERIENCES";
+  }
+  for (i = 0; i < coll.length; i++) {
+    var content = coll[i].nextElementSibling;
+    if (isAllOpen) {
+      coll[i].classList.add("active");
+      content.style.display = "block";
+    } else {
+      coll[i].classList.remove("active");
+      content.style.display = "none";
+    }
+  }
+}
+)
